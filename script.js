0f35331657194d5cfc1316ee76bf8ffdbdd8e43a
Bahnă David Beniamin <bahna.benu@yahoo.com>
2024-10-08 19:54:50 +0300

    I added some code to start displaying the weather

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const API_GEOLOCATION_URL = "https://geocoding-api.open-meteo.com/v1/search";
+const API_FORECAST_URL = "https://api.open-meteo.com/v1/forecast";
 
 const cityForm = document.querySelector("#cityForm");
 
@@ -23,6 +24,11 @@ async function onCityFormSubmit(event) {
     return;
   }
 
+  const weatherResponse = await getWeather(
+    cityCoordinates.lat,
+    cityCoordinates.long
+  );
+console.log(weatherResponse);
 
 }
 
@@ -42,4 +48,20 @@ async function getCityCoordinates(cityName) {
 
     const result = data.results[0];
     return {lat: result.latitude, long: result.longitude};
+}
+
+async function getWeather(lat, long) {
+   
+
+    const apiUrl = new URL(API_FORECAST_URL);
+    apiUrl.searchParams.append("latitude", lat);
+    apiUrl.searchParams.append("longitude", long);
+    apiUrl.searchParams.append("timezone", "auto");
+    apiUrl.searchParams.append("hourly", "temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m");
+
+    console.log(apiUrl.toString());
+
+    const response = await fetch(apiUrl.toString());
+    const data = await response.json();
+    return data;
 }
