03139f96d8d064c5e546964342a98fb860952355
Bahnă David Beniamin <bahna.benu@yahoo.com>
2024-10-10 22:55:21 +0300

    added new code in javaScript

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const API_GEOLOCATION_URL = "https://geocoding-api.open-meteo.com/v1/search";
 const API_FORECAST_URL = "https://api.open-meteo.com/v1/forecast";
 
 const cityForm = document.querySelector("#cityForm");
+const locationBtn = document.querySelector("#locationBtn");
 
 cityForm.addEventListener("submit", onCityFormSubmit);
+locationBtn.addEventListener("click," onLocationBtnClick);
 
 async function onCityFormSubmit(event) {
     event.preventDefault();
+
+    clearContent();
 
     const cityInput = cityForm.querySelector("#city");
     const cityName = cityInput.value.trim();
@@ -17,6 +21,9 @@ async function onCityFormSubmit(event) {
         alert("Enter the name of a city");
         return;
     }
+
+    displayLoading();
+
 
   const cityCoordinates = await getCityCoordinates(cityName);
   if(cityCoordinates === null) {
@@ -109,4 +116,83 @@ function parseApiData(data) {
 
 function dispkayWeather(cityName, weather) {
   const pageContent = document.querySelector(".page-content");
+
+  pageContent.append(createTodayWeatherSection(cityName, weather.current));
+  pageContent.append(createForecastWeatherSection(cityName, weather.forecast));
+}
+
+function createTodayWeatherSection(cityName, currentWeather) {
+  const todaySection = document.createElement('div');
+
+  const title = document.createElement('h2');
+  title.classList.add('section-title');
+  title.innerText = `Vremea in ${cityName} astazi`;
+
+  todaySection.append(title);
+
+  const weatherPanel = createWeatherPanel(currentWeather, true);
+  todaySection.append(weatherPanel);
+
+  return todaySection;
+}
+
+function createForecastWeatherSection(cityName, forecasts) {
+  const forecastSection = document.createElement('div');
+
+  const title = document.createElement('h2');
+  title.classList.add('section-title');
+  title.innerText = `Vremea in ${cityName} in urmatoarele zile`;
+  forecastSection.append(title);
+
+  const weatherItems = document.createElement('div');
+  weatherItems.classList.add('weather-items');
+  forecastSection.append(weatherItems);
+
+  for (let i = 0; i < forecasts.length; i++) {
+    const weatherPanel = createWeatherPanel(forecasts[i], false);
+    weatherItems.append(weatherPanel);
+  }
+
+  return forecastSection;
+}
+
+
+function createWeatherPanel(weather, isToday) {
+  const weatherPanel = document.createElement('div');
+  const panelClass = isToday ? 'today' : 'forecast';
+
+  weatherPanel.classList.add('weather-panel', panelClass);
+
+  const weatherDetails = document.createElement('div');
+  weatherDetails.classList.add('weather-details');
+  weatherPanel.append(weatherDetails);
+
+  const currentHour = new Date().getHours();
+  const isNight = currentHour >= 20 || currentHour <= 6;
+
+  const weatherIcon = getIcon(weather.code, isNight);
+
+  const imageContainer = document.createElement('div');
+  const icon = document.createElement('img');
+  icon.src = weatherIcon;
+
+  imageContainer.append(icon);
+  weatherPanel.append(imageContainer);
+
+  const date = document.createElement('p');
+  date.classList.add('date');
+  date.innerText = weather.date.replace('T', ', ');
+
+  const temp = document.createElement('p');
+  temp.innerText = `Temperatura: ${weather.temp}°C`;
+
+  const wind = document.createElement('p');
+  wind.innerText = `Vant: ${weather.wind} km/h`;
+
+  const humidity = document.createElement('p');
+  humidity.innerText = `Umiditate: ${weather.humidity} %`;
+
+  weatherDetails.append(date, temp, wind, humidity);
+
+  return weatherPanel;
 }
